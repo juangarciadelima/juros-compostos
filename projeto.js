177337479284191385964e08e00ecdfa4a215eab
Juan Garcia <juangarciadelima884@gmail.com>
2020-10-13 03:18:06 -0300

    Adicionando projeto de juros

---
+++ projeto.js
@@ -0,0 +1,13 @@
+const taxainicial = 15000
+const juros = 15
+const tempo = 12
+const taxamensal = 1000
+
+function juroscompostos(){
+
+      return taxainicial*juros*tempo*taxamensal*12
+
+}
+
+console.log ('juros', juroscompostos())
+      
